b947fe171ccf496ec73bca298b847bdd2961ffc9
offer <offer>
1998-03-04 02:13:12 +0000

    Added copyright statement

--- roms.c
+++ roms.c
@@ -1,9 +1,25 @@
 
 /*
-	$Id: roms.c,v 1.3 1998/01/29 17:31:18 offer Exp $
+	$Id: roms.c,v 1.4 1998/03/04 02:13:12 offer Exp $
 
-	(c) Richard M. Offer
+	Copyright (c) 1996-1998 Richard M. Offer
 
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+	
+Author: offer@sgi.com
 */
 
 #include <stdio.h>
